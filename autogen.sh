2ca010f3ad1aa485a297d618b7a549cee0331c10
Shaun McCance <shaunm@gnome.org>
2009-12-31 18:52:09 -0600

    Adding po directory to build and fixing dist issues

--- autogen.sh
+++ autogen.sh
@@ -5,7 +5,7 @@ srcdir=`dirname $0`
 test -z "$srcdir" && srcdir=.
 abs_srcdir=`(cd $srcdir && pwd)`
 
-PKG_NAME="gnome-doc-utils"
+PKG_NAME="yelp-xsl"
 
 (test -f $srcdir/configure.in \
   && test -f $srcdir/README \
@@ -19,8 +19,5 @@ which gnome-autogen.sh || {
     echo "You need to install gnome-common from the GNOME CVS"
     exit 1
 }
-
-# tools/ has gnome-doc-utils.m4 which is necessary to bootstrap g-d-u
-ACLOCAL_FLAGS="-I $abs_srcdir/tools $ACLOCAL_FLAGS"
 
 REQUIRED_AUTOMAKE_VERSION=1.9 USE_GNOME2_MACROS=1 . gnome-autogen.sh
